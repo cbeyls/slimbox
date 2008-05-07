3f8cbd60fd212b0821ae63faf2e5fe1a12a8436d
christophe.beyls <christophe.beyls@ad0b1a37-424c-0410-9f20-03b6aadad2d6>
2008-05-07 01:49:46 +0000

    Completely changed the organization of Slimbox code: Now only the public methods are exposed in the Slimbox object. All the private functions and variables are defined inside the scope of a global function. This has 2 benefits: - All the internal functions are completely hidden from the outside world; - Slimbox has its own scope so all the internal functions and variables can be referenced directly, contrary to the object model which required to add the "this." prefix everywhere, and required to bind callbacks to "this".
    This change drastically reduced the code size by about 700 bytes (!)

--- trunk/js/slimbox.js
+++ trunk/js/slimbox.js
@@ -4,14 +4,18 @@
 	Inspired by the original Lightbox v2 by Lokesh Dhakar.
 */
 
-var Slimbox = {
+var Slimbox = {};
 
-	anchors: [],
+(function() {
 
-	state: 0,	// State values: 0 (closed or closing), 1 (open and ready), 2+ (open and busy with animation)
+	var anchors = [], state = 0, options, images, activeImage, top, eventKeydown, fx, preload, preloadPrev, preloadNext;
+	// State values: 0 (closed or closing), 1 (open and ready), 2+ (open and busy with animation)
 
-	init: function(options) {
-		this.options = $extend({
+	var overlay, center, image, prevLink, nextLink, bottomContainer, bottom, caption, number;
+
+	// Slimbox initialization
+	window.addEvent("domready", function(_options) {
+		options = $extend({
 			resizeDuration: 400,			// Duration of each of the box resize animations (in milliseconds)
 			resizeTransition: false,		// Default transition in mootools
 			initialWidth: 250,			// Initial width of the box (in pixels)
@@ -19,79 +23,77 @@ var Slimbox = {
 			animateCaption: true,
 			showCounter: true,			// If true, a counter will only be shown if there is more than 1 image to display
 			counterText: "Image {x} of {y}"		// Translate or change as you wish
-		}, options || {});
+		}, _options || {});
 
 		$each(document.links, function(el) {
 			if (el.rel && el.rel.test(/^lightbox/i)) {
-				el.onclick = this.click.pass(el, this);
-				this.anchors.push(el);
+				el.onclick = click;
+				anchors.push(el);
 			}
-		}, this);
-		this.eventKeyDown = this.keyDown.bindAsEventListener(this);
-		this.eventPosition = this.position.bind(this);
+		});
+		eventKeyDown = keyDown.create({event: true});
 
-		this.overlay = new Element("div", {id: "lbOverlay"}).injectInside(document.body);
+		overlay = new Element("div", {id: "lbOverlay"}).injectInside(document.body);
 
-		this.center = new Element("div", {id: "lbCenter", styles: {width: this.options.initialWidth, height: this.options.initialHeight, marginLeft: -(this.options.initialWidth/2), display: "none"}}).injectInside(document.body);
-		this.image = new Element("div", {id: "lbImage"}).injectInside(this.center);
-		this.prevLink = new Element("a", {id: "lbPrevLink", href: "#", styles: {display: "none"}}).injectInside(this.image);
-		this.nextLink = this.prevLink.clone().setProperty("id", "lbNextLink").injectInside(this.image);
-		this.prevLink.onclick = this.previous.bind(this);
-		this.nextLink.onclick = this.next.bind(this);
+		center = new Element("div", {id: "lbCenter", styles: {width: options.initialWidth, height: options.initialHeight, marginLeft: -(options.initialWidth/2), display: "none"}}).injectInside(document.body);
+		image = new Element("div", {id: "lbImage"}).injectInside(center);
+		prevLink = new Element("a", {id: "lbPrevLink", href: "#", styles: {display: "none"}}).injectInside(image);
+		nextLink = prevLink.clone().setProperty("id", "lbNextLink").injectInside(image);
+		prevLink.onclick = previous;
+		nextLink.onclick = next;
 
-		this.bottomContainer = new Element("div", {id: "lbBottomContainer", styles: {display: "none"}}).injectInside(document.body);
-		this.bottom = new Element("div", {id: "lbBottom"}).injectInside(this.bottomContainer);
-		new Element("a", {id: "lbCloseLink", href: "#"}).injectInside(this.bottom).onclick = this.overlay.onclick = this.close.bind(this);
-		this.caption = new Element("div", {id: "lbCaption"}).injectInside(this.bottom);
-		this.number = new Element("div", {id: "lbNumber"}).injectInside(this.bottom);
-		new Element("div", {styles: {clear: "both"}}).injectInside(this.bottom);
+		bottomContainer = new Element("div", {id: "lbBottomContainer", styles: {display: "none"}}).injectInside(document.body);
+		bottom = new Element("div", {id: "lbBottom"}).injectInside(bottomContainer);
+		new Element("a", {id: "lbCloseLink", href: "#"}).injectInside(bottom).onclick = overlay.onclick = close;
+		caption = new Element("div", {id: "lbCaption"}).injectInside(bottom);
+		number = new Element("div", {id: "lbNumber"}).injectInside(bottom);
+		new Element("div", {styles: {clear: "both"}}).injectInside(bottom);
 
-		var nextEffect = this.nextEffect.bind(this);
-		this.fx = {
-			overlay: this.overlay.effect("opacity", {duration: 500}).hide(),
-			resize: this.center.effects($extend({duration: this.options.resizeDuration, onComplete: nextEffect}, this.options.resizeTransition ? {transition: this.options.resizeTransition} : {})),
-			image: this.image.effect("opacity", {duration: 500, onComplete: nextEffect}),
-			bottom: this.bottom.effect("margin-top", {duration: 400})
+		fx = {
+			overlay: overlay.effect("opacity", {duration: 500}).hide(),
+			resize: center.effects($extend({duration: options.resizeDuration, onComplete: nextEffect}, options.resizeTransition ? {transition: options.resizeTransition} : {})),
+			image: image.effect("opacity", {duration: 500, onComplete: nextEffect}),
+			bottom: bottom.effect("margin-top", {duration: 400})
 		};
 
-		this.preloadPrev = new Image();
-		this.preloadNext = new Image();
-	},
+		preloadPrev = new Image();
+		preloadNext = new Image();
+	});
 
-	click: function(link) {
+	function click() {
 		// Build the list of images that will be displayed
 		// startIndex is the image index related to the link that was clicked
-		var startIndex, images = [];
-		((link.rel == "lightbox") ? [link] : this.anchors).forEach(function(el, i) {
-			if (el.rel == link.rel) {
-				images.push([el.href, el.title]);
-				if (el.href == link.href) startIndex = i;
+		var imageNum, _images = [];
+		((this.rel == "lightbox") ? [this] : anchors).forEach(function(el, i) {
+			if (el.rel == this.rel) {
+				_images.push([el.href, el.title]);
+				if (el.href == this.href) imageNum = i;
 			}
 		}, this);
-		return this.open(images, startIndex);
-	},
+		return Slimbox.open(_images, imageNum);
+	}
 
-	open: function(images, startImage) {
+	Slimbox.open = function(_images, startImage) {
 		// The function is called for a single image, with URL and Title as first two arguments
-		if (typeof images == "string") {
-			images = [[images,startImage]];
+		if (typeof _images == "string") {
+			_images = [[_images,startImage]];
 			startImage = 0;
 		}
 
-		this.images = images;
-		this.position();
-		this.setup(true);
-		this.top = window.getScrollTop() + (window.getHeight() / 15);
-		this.center.setStyles({top: this.top, display: ""});
-		this.fx.overlay.start(0.8);
-		return this.changeImage(startImage);
-	},
+		images = _images;
+		position();
+		setup(true);
+		top = window.getScrollTop() + (window.getHeight() / 15);
+		center.setStyles({top: top, display: ""});
+		fx.overlay.start(0.8);
+		return changeImage(startImage);
+	};
 
-	position: function() {
-		this.overlay.setStyles({top: window.getScrollTop(), height: window.getHeight()});
-	},
+	function position() {
+		overlay.setStyles({top: window.getScrollTop(), height: window.getHeight()});
+	}
 
-	setup: function(open) {
+	function setup(open) {
 		var elements = $A(document.getElementsByTagName("object"));
 		elements.extend(document.getElementsByTagName(window.ie ? "select" : "embed"));
 		elements.each(function(el) {
@@ -99,100 +101,99 @@ var Slimbox = {
 			el.style.visibility = open ? "hidden" : el.lbBackupStyle;
 		});
 		var fn = open ? "addEvent" : "removeEvent";
-		window[fn]("scroll", this.eventPosition)[fn]("resize", this.eventPosition);
-		document[fn]("keydown", this.eventKeyDown);
-	},
+		window[fn]("scroll", position)[fn]("resize", position);
+		document[fn]("keydown", eventKeyDown);
+	}
 
-	keyDown: function(event) {
+	function keyDown(event) {
 		switch(event.keyCode) {
 			case 27:	// Esc
 			case 88:	// 'x'
 			case 67:	// 'c'
-				this.close();
+				close();
 				break;
 			case 37:	// Left arrow
 			case 80:	// 'p'
-				this.previous();
+				previous();
 				break;	
 			case 39:	// Right arrow
 			case 78:	// 'n'
-				this.next();
+				next();
 		}
-	},
+	}
 
-	previous: function() {
-		return this.changeImage(this.activeImage - 1);
-	},
+	function previous() {
+		return changeImage(activeImage - 1);
+	}
 
-	next: function() {
-		return this.changeImage(this.activeImage + 1);
-	},
+	function next() {
+		return changeImage(activeImage + 1);
+	}
 
-	changeImage: function(imageNum) {
-		if ((this.state > 1) || (imageNum < 0) || (imageNum >= this.images.length)) return false;
-		this.state = 2;
-		this.activeImage = imageNum;
+	function changeImage(imageNum) {
+		if ((state > 1) || (imageNum < 0) || (imageNum >= images.length)) return false;
+		state = 2;
+		activeImage = imageNum;
 
-		this.bottomContainer.style.display = this.prevLink.style.display = this.nextLink.style.display = "none";
-		this.fx.bottom.stop().set(0);
-		this.fx.image.hide();
-		this.center.className = "lbLoading";
+		bottomContainer.style.display = prevLink.style.display = nextLink.style.display = "none";
+		fx.bottom.stop().set(0);
+		fx.image.hide();
+		center.className = "lbLoading";
 
-		this.preload = new Image();
-		this.preload.onload = this.nextEffect.bind(this);
-		this.preload.src = this.images[imageNum][0];
-		return false;
-	},
-
-	nextEffect: function() {
-		switch (this.state++) {
-			case 2:
-				this.center.className = "";
-				this.image.style.backgroundImage = "url(" + this.images[this.activeImage][0] + ")";
-				this.image.style.width = this.bottom.style.width = this.preload.width + "px";
-				this.image.style.height = this.prevLink.style.height = this.nextLink.style.height = this.preload.height + "px";
-
-				this.caption.setHTML(this.images[this.activeImage][1] || "");
-				this.number.setHTML((this.options.showCounter && (this.images.length > 1)) ? this.options.counterText.replace(/{x}/, this.activeImage + 1).replace(/{y}/, this.images.length) : "");
-
-				if (this.activeImage) this.preloadPrev.src = this.images[this.activeImage - 1][0];
-				if (this.activeImage != (this.images.length - 1)) this.preloadNext.src = this.images[this.activeImage + 1][0];
-
-				if (this.center.clientHeight != this.image.offsetHeight) {
-					this.fx.resize.start({height: this.image.offsetHeight});
-					break;
-				}
-				this.state++;
-			case 3:
-				if (this.center.clientWidth != this.image.offsetWidth) {
-					this.fx.resize.start({width: this.image.offsetWidth, marginLeft: -this.image.offsetWidth/2});
-					break;
-				}
-				this.state++;
-			case 4:
-				this.bottomContainer.setStyles({top: this.top + this.center.clientHeight, height: 0, marginLeft: this.center.style.marginLeft, display: ""});
-				this.fx.image.start(1);
-				break;
-			case 5:
-				if (this.activeImage) this.prevLink.style.display = "";
-				if (this.activeImage != (this.images.length - 1)) this.nextLink.style.display = "";
-				if (this.options.animateCaption) {
-					this.fx.bottom.set(-this.bottom.offsetHeight).start(0);
-				}
-				this.bottomContainer.style.height = "";
-				this.state = 1;
-		}
-	},
-
-	close: function() {
-		if (!this.state) return;
-		this.state = 0;
-		if (this.preload) this.preload.onload = Class.empty;
-		for (var f in this.fx) this.fx[f].stop();
-		this.center.style.display = this.bottomContainer.style.display = "none";
-		this.fx.overlay.chain(this.setup.pass(false, this)).start(0);
+		preload = new Image();
+		preload.onload = nextEffect;
+		preload.src = images[imageNum][0];
 		return false;
 	}
-};
 
-window.addEvent("domready", Slimbox.init.bind(Slimbox));
+	function nextEffect() {
+		switch (state++) {
+			case 2:
+				center.className = "";
+				image.style.backgroundImage = "url(" + images[activeImage][0] + ")";
+				image.style.width = bottom.style.width = preload.width + "px";
+				image.style.height = prevLink.style.height = nextLink.style.height = preload.height + "px";
+
+				caption.setHTML(images[activeImage][1] || "");
+				number.setHTML((options.showCounter && (images.length > 1)) ? options.counterText.replace(/{x}/, activeImage + 1).replace(/{y}/, images.length) : "");
+
+				if (activeImage) preloadPrev.src = images[activeImage - 1][0];
+				if (activeImage != (images.length - 1)) preloadNext.src = images[activeImage + 1][0];
+
+				if (center.clientHeight != image.offsetHeight) {
+					fx.resize.start({height: image.offsetHeight});
+					break;
+				}
+				state++;
+			case 3:
+				if (center.clientWidth != image.offsetWidth) {
+					fx.resize.start({width: image.offsetWidth, marginLeft: -image.offsetWidth/2});
+					break;
+				}
+				state++;
+			case 4:
+				bottomContainer.setStyles({top: top + center.clientHeight, height: 0, marginLeft: center.style.marginLeft, display: ""});
+				fx.image.start(1);
+				break;
+			case 5:
+				if (activeImage) prevLink.style.display = "";
+				if (activeImage != (images.length - 1)) nextLink.style.display = "";
+				if (options.animateCaption) {
+					fx.bottom.set(-bottom.offsetHeight).start(0);
+				}
+				bottomContainer.style.height = "";
+				state = 1;
+		}
+	}
+
+	function close() {
+		if (!state) return;
+		state = 0;
+		if (preload) preload.onload = Class.empty;
+		for (var f in fx) fx[f].stop();
+		center.style.display = bottomContainer.style.display = "none";
+		fx.overlay.chain(setup.pass(false)).start(0);
+		return false;
+	}
+
+})();
