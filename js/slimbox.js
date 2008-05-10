a0f1bceb0023b50f9fe995df82c048ef25d45f95
christophe.beyls <christophe.beyls@ad0b1a37-424c-0410-9f20-03b6aadad2d6>
2008-05-10 01:21:37 +0000

    changed version number as final version 1.5

--- js/slimbox.js
+++ js/slimbox.js
@@ -1,5 +1,5 @@
 /*!
-	Slimbox v1.5-dev - The ultimate lightweight Lightbox clone
+	Slimbox v1.5 - The ultimate lightweight Lightbox clone
 	(c) 2007-2008 Christophe Beyls <http://www.digitalia.be>
 	MIT-style license.
 */
