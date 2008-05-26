4c28001e29694b62f0cac0cec02160871e68438f
christophe.beyls <christophe.beyls@ad0b1a37-424c-0410-9f20-03b6aadad2d6>
2008-05-26 17:47:29 +0000

    Tagging v1.6 for mootools 1.2

---
+++ tags/1.6/js/autoload.js
@@ -0,0 +1,11 @@
+// AUTOLOAD CODE BLOCK (MAY BE CHANGED OR REMOVED)
+Slimbox.scanPage = function() {
+	var links = $$("a").filter(function(el) {
+		return el.rel && el.rel.test(/^lightbox/i);
+	});
+	// PUT YOUR CUSTOM OPTIONS INSIDE THE FOLLOWING {}
+	$$(links).slimbox({}, null, function(el) {
+		return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
+	});
+};
+window.addEvent("domready", Slimbox.scanPage);
