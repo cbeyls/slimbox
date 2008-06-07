a76009418ce134e8f78d17271cc1ab273ead3d92
christophe.beyls <christophe.beyls@ad0b1a37-424c-0410-9f20-03b6aadad2d6>
2008-06-07 23:41:32 +0000

    Removed obsolete comment

--- branches/1.6/js/autoload.js
+++ branches/1.6/js/autoload.js
@@ -3,7 +3,6 @@ Slimbox.scanPage = function() {
 	var links = $$("a").filter(function(el) {
 		return el.rel && el.rel.test(/^lightbox/i);
 	});
-	// PUT YOUR CUSTOM OPTIONS INSIDE THE FOLLOWING {}
 	$$(links).slimbox({/* Put custom options here */}, null, function(el) {
 		return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
 	});
