85f5e246ca44c445aee51357330481aa9bb90f10
christophe.beyls <christophe.beyls@ad0b1a37-424c-0410-9f20-03b6aadad2d6>
2008-11-27 17:41:33 +0000

    Upgraded trunk to latest version of the previous 1.6 branch (1.65)

--- trunk/js/slimbox.js
+++ trunk/js/slimbox.js
@@ -1,5 +1,5 @@
 /*!
-	Slimbox v1.6 - The ultimate lightweight Lightbox clone
+	Slimbox v1.65 - The ultimate lightweight Lightbox clone
 	(c) 2007-2008 Christophe Beyls <http://www.digitalia.be>
 	MIT-style license.
 */
@@ -9,7 +9,7 @@ var Slimbox;
 (function() {
 
 	// Global variables, accessible to Slimbox only
-	var state = 0, options, images, activeImage, top, fx, preload, preloadPrev = new Image(), preloadNext = new Image(),
+	var state = 0, options, images, activeImage, prevImage, nextImage, top, fx, preload, preloadPrev = new Image(), preloadNext = new Image(),
 	// State values: 0 (closed or closing), 1 (open and ready), 2+ (open and busy with animation)
 
 	// DOM elements
@@ -23,27 +23,23 @@ var Slimbox;
 		// Append the Slimbox HTML code at the bottom of the document
 		$(document.body).adopt(
 			$$([
-				overlay = new Element("div", {id: "lbOverlay"}),
+				overlay = new Element("div", {id: "lbOverlay"}).addEvent("click", close),
 				center = new Element("div", {id: "lbCenter"}),
 				bottomContainer = new Element("div", {id: "lbBottomContainer"})
 			]).setStyle("display", "none")
 		);
 
 		image = new Element("div", {id: "lbImage"}).injectInside(center).adopt(
-			prevLink = new Element("a", {id: "lbPrevLink", href: "#"}),
-			nextLink = new Element("a", {id: "lbNextLink", href: "#"})
+			prevLink = new Element("a", {id: "lbPrevLink", href: "#"}).addEvent("click", previous),
+			nextLink = new Element("a", {id: "lbNextLink", href: "#"}).addEvent("click", next)
 		);
-		prevLink.onclick = previous;
-		nextLink.onclick = next;
 
-		var closeLink;
 		bottom = new Element("div", {id: "lbBottom"}).injectInside(bottomContainer).adopt(
-			closeLink = new Element("a", {id: "lbCloseLink", href: "#"}),
+			new Element("a", {id: "lbCloseLink", href: "#"}).addEvent("click", close),
 			caption = new Element("div", {id: "lbCaption"}),
 			number = new Element("div", {id: "lbNumber"}),
 			new Element("div", {styles: {clear: "both"}})
 		);
-		closeLink.onclick = overlay.onclick = close;
 
 		fx = {
 			overlay: new Fx.Tween(overlay, {property: "opacity", duration: 500}).set(0),
@@ -60,6 +56,7 @@ var Slimbox;
 	Slimbox = {
 		open: function(_images, startImage, _options) {
 			options = $extend({
+				loop: false,				// Allows to navigate between first and last images
 				overlayOpacity: 0.8,			// 1 is opaque, 0 is completely transparent (change the color in the CSS file)
 				resizeDuration: 400,			// Duration of each of the box resize animations (in milliseconds)
 				resizeTransition: false,		// Default transition in mootools
@@ -77,12 +74,14 @@ var Slimbox;
 			}
 
 			images = _images;
+			options.loop = options.loop && (images.length > 1);
 			position();
 			setup(true);
 			top = window.getScrollTop() + (window.getHeight() / 15);
 			fx.resize = new Fx.Morph(center, $extend({duration: options.resizeDuration, onComplete: nextEffect}, options.resizeTransition ? {transition: options.resizeTransition} : {}));
 			center.setStyles({top: top, width: options.initialWidth, height: options.initialHeight, marginLeft: -(options.initialWidth/2), display: ""});
 			fx.overlay.start(options.overlayOpacity);
+			state = 1;
 			return changeImage(startImage);
 		}
 	};
@@ -91,6 +90,8 @@ var Slimbox;
 		slimbox: function(_options, linkMapper) {
 			// The processing of a single element is similar to the processing of a collection with a single element
 			$$(this).slimbox(_options, linkMapper);
+
+			return this;
 		}
 	});
 
@@ -114,13 +115,13 @@ var Slimbox;
 
 			var links = this;
 
-			links.forEach(function(link) {
-				link.onclick = function() {
-					// Build the list of images that will be displayed
-					var filteredLinks = links.filter(linksFilter, this);
-					return Slimbox.open(filteredLinks.map(linkMapper), filteredLinks.indexOf(this), _options);
-				};
+			links.removeEvents("click").addEvent("click", function() {
+				// Build the list of images that will be displayed
+				var filteredLinks = links.filter(linksFilter, this);
+				return Slimbox.open(filteredLinks.map(linkMapper), filteredLinks.indexOf(this), _options);
 			});
+
+			return links;
 		}
 	});
 
@@ -134,9 +135,11 @@ var Slimbox;
 	}
 
 	function setup(open) {
-		$$("object", Browser.Engine.trident ? "select" : "embed").forEach(function(el) {
-			if (open) el.slimbox = el.style.visibility;
-			el.style.visibility = open ? "hidden" : el.slimbox;
+		["object", Browser.Engine.trident ? "select" : "embed"].forEach(function(tag) {
+			Array.forEach(document.getElementsByTagName(tag), function(el) {
+				if (open) el._slimbox = el.style.visibility;
+				el.style.visibility = open ? "hidden" : el._slimbox;
+			});
 		});
 
 		overlay.style.display = open ? "" : "none";
@@ -166,26 +169,30 @@ var Slimbox;
 	}
 
 	function previous() {
-		return changeImage(activeImage - 1);
+		return changeImage(prevImage);
 	}
 
 	function next() {
-		return changeImage(activeImage + 1);
+		return changeImage(nextImage);
 	}
 
 	function changeImage(imageIndex) {
-		if ((state > 1) || (imageIndex < 0) || (imageIndex >= images.length)) return false;
-		state = 2;
-		activeImage = imageIndex;
+		if ((state == 1) && (imageIndex >= 0)) {
+			state = 2;
+			activeImage = imageIndex;
+			prevImage = ((activeImage || !options.loop) ? activeImage : images.length) - 1;
+			nextImage = activeImage + 1;
+			if (nextImage == images.length) nextImage = options.loop ? 0 : -1;
 
-		$$(prevLink, nextLink, image, bottomContainer).setStyle("display", "none");
-		fx.bottom.cancel().set(0);
-		fx.image.set(0);
-		center.className = "lbLoading";
+			$$(prevLink, nextLink, image, bottomContainer).setStyle("display", "none");
+			fx.bottom.cancel().set(0);
+			fx.image.set(0);
+			center.className = "lbLoading";
 
-		preload = new Image();
-		preload.onload = nextEffect;
-		preload.src = images[imageIndex][0];
+			preload = new Image();
+			preload.onload = nextEffect;
+			preload.src = images[imageIndex][0];
+		}
 
 		return false;
 	}
@@ -201,8 +208,8 @@ var Slimbox;
 				caption.set('html', images[activeImage][1] || "");
 				number.set('html', (options.showCounter && (images.length > 1)) ? options.counterText.replace(/{x}/, activeImage + 1).replace(/{y}/, images.length) : "");
 
-				if (activeImage) preloadPrev.src = images[activeImage - 1][0];
-				if (activeImage != (images.length - 1)) preloadNext.src = images[activeImage + 1][0];
+				if (prevImage >= 0) preloadPrev.src = images[prevImage][0];
+				if (nextImage >= 0) preloadNext.src = images[nextImage][0];
 
 				if (center.clientHeight != image.offsetHeight) {
 					fx.resize.start({height: image.offsetHeight});
@@ -216,27 +223,28 @@ var Slimbox;
 				}
 				state++;
 			case 4:
-				bottomContainer.setStyles({top: top + center.clientHeight, height: 0, marginLeft: center.style.marginLeft, display: ""});
+				bottomContainer.setStyles({top: top + center.clientHeight, marginLeft: center.style.marginLeft, visibility: "hidden", display: ""});
 				fx.image.start(1);
 				break;
 			case 5:
-				if (activeImage) prevLink.style.display = "";
-				if (activeImage < (images.length - 1)) nextLink.style.display = "";
+				if (prevImage >= 0) prevLink.style.display = "";
+				if (nextImage >= 0) nextLink.style.display = "";
 				if (options.animateCaption) {
 					fx.bottom.set(-bottom.offsetHeight).start(0);
 				}
-				bottomContainer.style.height = "";
+				bottomContainer.style.visibility = "";
 				state = 1;
 		}
 	}
 
 	function close() {
-		if (!state) return false;
-		state = 0;
-		preload.onload = $empty;
-		for (var f in fx) fx[f].cancel();
-		$$(center, bottomContainer).setStyle("display", "none");
-		fx.overlay.chain(setup).start(0);
+		if (state) {
+			state = 0;
+			preload.onload = $empty;
+			for (var f in fx) fx[f].cancel();
+			$$(center, bottomContainer).setStyle("display", "none");
+			fx.overlay.chain(setup).start(0);
+		}
 
 		return false;
 	}
