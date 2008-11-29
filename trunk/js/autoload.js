5a1fcf7459f51bab94522cad458e3a86815c6353
christophe.beyls <christophe.beyls@ad0b1a37-424c-0410-9f20-03b6aadad2d6>
2008-11-29 05:47:30 +0000

    shorter autoload code block

--- trunk/js/autoload.js
+++ trunk/js/autoload.js
@@ -1,9 +1,8 @@
 // AUTOLOAD CODE BLOCK (MAY BE CHANGED OR REMOVED)
 Slimbox.scanPage = function() {
-	var links = $$("a").filter(function(el) {
+	$$("a").filter(function(el) {
 		return el.rel && el.rel.test(/^lightbox/i);
-	});
-	$$(links).slimbox({/* Put custom options here */}, null, function(el) {
+	}).slimbox({/* Put custom options here */}, null, function(el) {
 		return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
 	});
 };
