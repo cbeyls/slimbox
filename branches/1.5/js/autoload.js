89d1182069a76dcb4e0e4b36082bee4636e50927
christophe.beyls <christophe.beyls@ad0b1a37-424c-0410-9f20-03b6aadad2d6>
2008-11-29 08:17:13 +0000

    Using document.links instead of "a" to include both <a> and <area> tags without using a selector.

--- branches/1.5/js/autoload.js
+++ branches/1.5/js/autoload.js
@@ -1,6 +1,6 @@
 // AUTOLOAD CODE BLOCK (MAY BE CHANGED OR REMOVED)
 Slimbox.scanPage = function() {
-	$$($$("a").filter(function(el) {
+	$$($$(document.links).filter(function(el) {
 		return el.rel && el.rel.test(/^lightbox/i);
 	})).slimbox({/* Put custom options here */}, null, function(el) {
 		return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
