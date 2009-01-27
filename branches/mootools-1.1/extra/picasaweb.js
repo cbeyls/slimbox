8e0880a32385e992ec1ff3cec9f2d708c11039a7
christophe.beyls <christophe.beyls@ad0b1a37-424c-0410-9f20-03b6aadad2d6>
2009-01-27 19:15:59 +0000

    Added support for square image sizes in the Picasa Web Albums example script, and changed the images size to make it similar to the Flickr medium image size.

--- branches/mootools-1.1/extra/picasaweb.js
+++ branches/mootools-1.1/extra/picasaweb.js
@@ -13,7 +13,7 @@ window.addEvent("domready", function() {
 	$$($$("a").filter(function(el) {
 		return el.href && !el.href.indexOf("http://picasaweb.google.") && el.firstChild && el.firstChild.src;
 	})).slimbox({}, function(el) {
-		return [el.firstChild.src.replace(/\/s\d+\/([^\/]+)$/, "/s640/$1"),
+		return [el.firstChild.src.replace(/\/s\d+(?:\-c)?\/([^\/]+)$/, "/s512/$1"),
 			(el.title || el.firstChild.alt) + '<br /><a href="' + el.href + '">Picasa Web Albums page</a>'];
 	});
 });
