9a984e8eb69dd5cb46b24c0faccabc7345c7f1e4
christophe.beyls <christophe.beyls@ad0b1a37-424c-0410-9f20-03b6aadad2d6>
2009-01-28 03:06:47 +0000

    Tagging version 1.57

--- extra/picasaweb.js
+++ extra/picasaweb.js
@@ -9,8 +9,10 @@
 	Add the following code to the autoload code block.
 */
 
-jQuery(function($) {
-	$("a[href^='http://picasaweb.google.'] > img:first-child[src]").parent().slimbox({}, function(el) {
+window.addEvent("domready", function() {
+	$$($$("a").filter(function(el) {
+		return el.href && !el.href.indexOf("http://picasaweb.google.") && el.firstChild && el.firstChild.src;
+	})).slimbox({}, function(el) {
 		return [el.firstChild.src.replace(/\/s\d+(?:\-c)?\/([^\/]+)$/, "/s512/$1"),
 			(el.title || el.firstChild.alt) + '<br /><a href="' + el.href + '">Picasa Web Albums page</a>'];
 	});
