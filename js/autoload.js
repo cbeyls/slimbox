14bd76d8f31d49e86e0cd4f9b4f39db54790b589
christophe.beyls@gmail.com <christophe.beyls@gmail.com@ad0b1a37-424c-0410-9f20-03b6aadad2d6>
2009-11-18 21:47:13 +0000

    Activate Slimbox on the "a" tags only by default, not on the anchor tags because the "rel" attribute is not valid on these tags.

--- js/autoload.js
+++ js/autoload.js
@@ -1,6 +1,6 @@
 // AUTOLOAD CODE BLOCK (MAY BE CHANGED OR REMOVED)
 Slimbox.scanPage = function() {
-	$$(document.links).filter(function(el) {
+	$$("a").filter(function(el) {
 		return el.rel && el.rel.test(/^lightbox/i);
 	}).slimbox({/* Put custom options here */}, null, function(el) {
 		return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
