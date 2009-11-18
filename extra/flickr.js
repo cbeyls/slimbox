31c3d6ce69ea428fa1c2e12d4bec2e065d87e3a1
christophe.beyls@gmail.com <christophe.beyls@gmail.com@ad0b1a37-424c-0410-9f20-03b6aadad2d6>
2009-11-18 23:08:04 +0000

    Tagging release 1.58

--- extra/flickr.js
+++ extra/flickr.js
@@ -10,10 +10,12 @@
 */
 
 if (!/android|iphone|ipod|series60|symbian|windows ce|blackberry/i.test(navigator.userAgent)) {
-    jQuery(function($) {
-        $("a[href^='http://www.flickr.com/photos/'] > img:first-child[src]").parent().slimbox({}, function(el) {
-            return [el.firstChild.src.replace(/_[mts]\.(\w+)$/, ".$1"),
-                (el.title || el.firstChild.alt) + '<br /><a href="' + el.href + '">Flickr page</a>'];
-        });
-    });
+	window.addEvent("domready", function() {
+		$$($$("a").filter(function(el) {
+			return el.href && !el.href.indexOf("http://www.flickr.com/photos/") && el.firstChild && el.firstChild.src;
+		})).slimbox({}, function(el) {
+			return [el.firstChild.src.replace(/_[mts]\.(\w+)$/, ".$1"),
+				(el.title || el.firstChild.alt) + '<br /><a href="' + el.href + '">Flickr page</a>'];
+		});
+	});
 }
