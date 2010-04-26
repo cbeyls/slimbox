cdcf5f873012710a41e34496487779c7d36db443
christophe.beyls <christophe.beyls@ad0b1a37-424c-0410-9f20-03b6aadad2d6>
2010-04-26 23:35:39 +0000

    Replaced spaces with tabulations.

--- extra/picasaweb.js
+++ extra/picasaweb.js
@@ -10,10 +10,10 @@
 */
 
 if (!/android|iphone|ipod|series60|symbian|windows ce|blackberry/i.test(navigator.userAgent)) {
-    jQuery(function($) {
-        $("a[href^='http://picasaweb.google.'] > img:first-child[src]").parent().slimbox({}, function(el) {
-            return [el.firstChild.src.replace(/\/s\d+(?:\-c)?\/([^\/]+)$/, "/s512/$1"),
-                (el.title || el.firstChild.alt) + '<br /><a href="' + el.href + '">Picasa Web Albums page</a>'];
-        });
-    });
+	jQuery(function($) {
+		$("a[href^='http://picasaweb.google.'] > img:first-child[src]").parent().slimbox({}, function(el) {
+			return [el.firstChild.src.replace(/\/s\d+(?:\-c)?\/([^\/]+)$/, "/s512/$1"),
+				(el.title || el.firstChild.alt) + '<br /><a href="' + el.href + '">Picasa Web Albums page</a>'];
+		});
+	});
 }
